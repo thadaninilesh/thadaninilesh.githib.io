bbb7e9d02a0d6a707052a4c931b7ff0d89214038
Nilesh Thadani <thadaninilesh@gmail.com>
2016-04-19 11:57:23 +0530

    profile

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Nilesh Thadani's Profile
+        </title>
+        <style>
+            .im {
+            background: url(Nilesh-Thadani-Resume.jpg) no-repeat center; 
+           
+            }
+        </style>
+    </head>
+    <body>
+        <img src="Nilesh-Thadani-Resume.jpg" style="width:100%">
+        </body>
+</html>
